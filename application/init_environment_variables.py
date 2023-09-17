8e50916a057d7658f583a169af3dfd69b120198b
Chris Thomas <71943070+ChrisThomas0@users.noreply.github.com>
2023-09-17 20:27:06 +0100

    Update init_environment_variables.py

--- application/init_environment_variables.py
+++ application/init_environment_variables.py
@@ -1,6 +1,6 @@
 import openai
 
-PROJECT_PATH = "https://github.com/ChrisThomas0/Simplify/tree/51d080668cf5d1d838c3876f40692b98a55eee38/dataset"
+PROJECT_PATH = "../dataset"
 API_KEY = "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"
 ORG_KEY = "org-VUUQolpC4g6Xn0hYFeH534xT"
 openai.api_key = API_KEY
