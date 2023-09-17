51d080668cf5d1d838c3876f40692b98a55eee38
Chris Thomas <71943070+ChrisThomas0@users.noreply.github.com>
2023-09-17 20:11:39 +0100

    Delete deprecated-code-base directory

--- deprecated-code-base/init_environment_variables.py
+++
@@ -1,7 +0,0 @@
-import openai
-
-PROJECT_PATH = "/Users/ceadar/Documents/WallaceCorp/Project/"
-API_KEY = "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"
-ORG_KEY = "org-VUUQolpC4g6Xn0hYFeH534xT"
-openai.api_key = API_KEY
-openai.organization = ORG_KEY
